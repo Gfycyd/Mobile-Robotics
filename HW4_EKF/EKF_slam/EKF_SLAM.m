02b1c8776687dec7fb83ef71251ad46ca25f5fa7
Gfycyd <31628809+Gfycyd@users.noreply.github.com>
2020-09-28 23:11:08 +0300

    EKF Slam prediction

--- HW4_EKF/EKF_slam/EKF_SLAM.m
+++ HW4_EKF/EKF_slam/EKF_SLAM.m
@@ -1,12 +1,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%  16833 Robot Localization and Mapping  % 
+%  16833 Robot Localization and Mapping  %
 %  Assignment #2                         %
 %  EKF-SLAM                              %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear;
 close all;
+clc;
 
-%==== TEST: Setup uncertianty parameters (try different values!) ===
+%==== TEST: Setup uncertainity parameters (try different values!) ===
 sig_x = 0.25;
 sig_y = 0.1;
 sig_alpha = 0.1;
@@ -40,33 +41,25 @@ pose_cov = diag([0.02^2, 0.02^2, 0.1^2]);
 %==== Setup initial landmark vector landmark[] and covariance matrix landmark_cov[] ====
 %==== (Hint: use initial pose with uncertainty and first measurement) ====
 
-% Write your code here...
-k = 6;
-landmark_cov = zeros(2*k);
-landmark = zeros(2*k, 1);
+number_of_landmarks = 6;
+landmark = zeros(number_of_landmarks*2,1);
+landmark_cov = zeros(number_of_landmarks*2,number_of_landmarks*2);
 
-for i = 1:6
-    r = measure(2*i);
-    beta = measure(2*i-1);
+% for vizualization in report and debugging:
+% landmark_cov = diag([0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3]);
 
-    J_cov = [-r*sin(beta) cos(beta);
-              r*cos(beta) sin(beta)];
-
-    landmark_cov(2*i-1:2*i, 2*i-1:2*i) = J_cov*measure_cov*J_cov;
-    landmark(2*i-1) = measure(2*i)*cos(measure(2*i-1));
-    landmark(2*i) = measure(2*i)*sin(measure(2*i-1));
+for i = 1:number_of_landmarks
+    landmark(2*i-1:2*i,1)= [measure(2*i)*cos(measure(2*i-1));measure(2*i)*sin(measure(2*i-1))];
+    R = [-measure(2*i)*sin(measure(2*i-1)) cos(measure(2*i-1)); measure(2*i)*cos(measure(2*i-1)) sin(measure(2*i-1))];
+    landmark_cov(2*i-1:2*i, 2*i-1:2*i) = R*measure_cov*R;
 end
-
-l_cov=0.4;
-landmark_cov = diag([l_cov,l_cov,l_cov,l_cov,l_cov,l_cov,l_cov,l_cov,l_cov,l_cov,l_cov,l_cov]);
-
 %==== Setup state vector x with pose and landmark vector ====
 x = [pose ; landmark];
 
 %==== Setup covariance matrix P with pose and landmark covariances ====
-P = [pose_cov zeros(3, 2*k) ; zeros(2*k, 3) landmark_cov];
+P = [pose_cov zeros(3, 2*number_of_landmarks) ; zeros(2*number_of_landmarks, 3) landmark_cov];
 
-%==== Plot initial state and covariance ====
+%==== Plot initial state and conariance ====
 last_x = x;
 drawTrajAndMap(x, last_x, P, 0);
 
@@ -76,26 +69,17 @@ while ischar(tline)
     arr = str2num(tline);
     d = arr(1);
     alpha = arr(2);
+    at = x(3);
     
     %==== Predict Step ====
     %==== (Notice: predict state x_pre[] and covariance P_pre[] using input control data and control_cov[]) ====
-    F = zeros(3+2*k,3);
+    F = zeros(3+2*number_of_landmarks,3);
     F(1:3,1:3)  = eye(3);
 
-    % Write your code here...
-    x_pre = x + F*[d*cos(x(3));
-                   d*sin(x(3)); 
-                   alpha];
-    %x_pre(3) = wrapToPi(x_pre(3));
+    x_pre = x + F*[d*cos(at);d*sin(at); alpha];
+    F_p = [[1 0 -d*sin(alpha); 0 1 d*cos(alpha); 0 0   1] zeros(3, 2*number_of_landmarks); zeros(2*number_of_landmarks, 3) eye(2*number_of_landmarks)];
 
-    J_m = [1, 0 , -d*sin(x(3));
-           0, 1 , d*cos(x(3));
-           0, 0   1];
-
-    G_t = [J_m zeros(3, 2*k); zeros(2*k, 3) eye(2*k)];
-
-    P_pre =  G_t*P*G_t' + F*control_cov*F';
-    
+    P_pre =  F_p*P*F_p' + F*control_cov*F';
     %==== Draw predicted state x_pre[] and covariance P_pre[] ====
     drawTrajPre(x_pre, P_pre);
     
@@ -105,8 +89,8 @@ while ischar(tline)
     measure = arr';
 
 
-    %==== Update Step ====    
-    for i=1:2:2*k-1
+    %==== Update Step ====
+    for i=1:2:2*number_of_landmarks-1
         
         delta_x = x_pre(3+i)-x_pre(1);
         delta_y = x_pre(3+i+1)-x_pre(2);
@@ -115,7 +99,7 @@ while ischar(tline)
         beta_pred = wrapToPi(atan2(delta_y, delta_x) - x_pre(3));
         z_pred = [q;beta_pred];
         
-        F = zeros(5,3+2*k);
+        F = zeros(5,3+2*number_of_landmarks);
         F(1:3,1:3)=eye(3);
         F(4:5, 3+i:3+i+1)=eye(2);
         
@@ -130,7 +114,7 @@ while ischar(tline)
         z_actual = [measure(i+1);measure(i)];
         
         x_pre = x_pre + K*(z_actual-z_pred);
-        P_pre= (eye(3+2*k)-K*H)*P_pre;
+        P_pre= (eye(3+2*number_of_landmarks)-K*H)*P_pre;
         
           
     
@@ -139,7 +123,7 @@ while ischar(tline)
     x = x_pre;
     P = P_pre;
 
-    %==== Plot ====   
+    %==== Plot ====
     drawTrajAndMap(x, last_x, P, t);
     last_x = x;
     
@@ -147,9 +131,8 @@ while ischar(tline)
     t = t + 1;
     tline = fgets(fid);
 end
-
-
 %==== EVAL: Plot ground truth landmarks ====
+
 
 % plot true landmarks
 ground_truth = [3 6; 3 12; 7 8; 7 14; 11 6; 11 12];
@@ -165,16 +148,17 @@ disp(euclidean_distance)
 
 
 % calculate the Mahalanobis distances
-gt_x = [ 3 3 7 7 11 11];
-gt_y = [6 12 8 14 6 12];
 
-mahal = zeros(1,6);
+mahalanobis_distance = [0 0 0 0 0 0];
 
-for i = 1:6
-    e = [ gt_x(i)-x(3+i*2-1);gt_y(i)-x(3+i*2)];
-    sigma = P(3+2*i-1:3+2*i,3+2*i-1:3+2*i);
-    mahal(i) = sqrt(e'*sigma*e);
+for i = 1:number_of_landmarks
+    b_1_index = ground_truth(i,1)-x(3+i*2-1);
+    b_2_index = ground_truth(i,2)-x(3+i*2);
+    b = [b_1_index; b_2_index];
+    mahalanobis_distance(i) = sqrt(b'*P(3+2*i-1:3+2*i,3+2*i-1:3+2*i)*b);
 
 end
+disp(mahalanobis_distance)
+
 %==== Close data file ====
 fclose(fid);
